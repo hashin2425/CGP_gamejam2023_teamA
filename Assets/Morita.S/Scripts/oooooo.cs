eb12756ddc6406bf9c4b615faa0089aa0cb4b83e
shingomorita1234 <antenakakko@gmail.com>
2024-03-04 16:53:46 +0900

    あ

---
+++ Assets/Morita.S/Scripts/oooooo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static GameData.ConstSettings;
+
+public class oooooo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       // PlayerPrefs.GetInt(Items.Mouse.ToString(), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
